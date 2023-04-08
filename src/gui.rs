397b42f9041e50e6941ef7b4135d120b16daba62
hacknus <linus.stoeckli@unibe.ch>
2023-04-08 10:11:36 +0200

    implement keyboard shortcuts for csv and plot saving

--- src/gui.rs
+++ src/gui.rs
@@ -483,9 +483,17 @@ impl MyApp {
                                 }
                             });
                             ui.end_row();
-                            if ui.button("Save CSV")
+
+                            let save_file_shortcut =
+                                egui::KeyboardShortcut::new(egui::Modifiers::COMMAND, egui::Key::S);
+
+                            let save_plot_shortcut =
+                                egui::KeyboardShortcut::new(egui::Modifiers::COMMAND | egui::Modifiers::SHIFT, egui::Key::S);
+
+                            if ui.button("Save CSV") 
                                 .on_hover_text("Save plot data to CSV.")
-                                .clicked() {
+                                .clicked() || ui.input_mut(|i| i.consume_shortcut(&save_file_shortcut))
+                            {
                                 if let Some(path) = rfd::FileDialog::new().save_file() {
                                     self.picked_path = path;
                                     self.picked_path.set_extension("csv");
@@ -506,7 +514,7 @@ impl MyApp {
                             if ui
                                 .button("Save Plot")
                                 .on_hover_text("Save an image of the plot - this is experimental!")
-                                .clicked()
+                                .clicked() || ui.input_mut(|i| i.consume_shortcut(&save_plot_shortcut))
                             {
                                 if let Some(mut path) = rfd::FileDialog::new().save_file() {
                                     path.set_extension("png");
